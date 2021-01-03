8697ffb814d624b5b5e8eae8b0faf3be6c41b280
VolVox99 <alirastegar123@gmail.com>
2021-01-02 20:17:49 -0800

    improved commands code

--- c_repl/__main__.py
+++ c_repl/__main__.py
@@ -1,4 +1,8 @@
-from c_repl.repl import main
+from c_repl.Repl import Repl
+
+def main():
+    Repl().run()
+
 
 if __name__ == '__main__':
     main()
--- c_repl/repl.py
+++ c_repl/repl.py
@@ -1,9 +1,40 @@
-from c_repl.UI import Repl
+import sys
+from c_repl.executer import Executer
 
-def main():
-    Repl().run()
+class Repl:
+    def __init__(self):
+        self.executer = Executer()
+        self.command_start = '/'
+        self._commands = {
+            'quit': lambda: sys.exit(),
+            'reset': lambda: self.executer.reset(),
+        }
+
+    @staticmethod
+    def clean(code):
+        return code.strip().replace('\n', ' ')
+    
+    @property
+    def commands(self):
+        return {self.command_start + k: v for k, v in self._commands.items()}
+
+    def execute_command(self, command, *args, **kwargs):
+        return self.commands[self.command_start + command](*args, **kwargs)
+
+    def check_for_command(self, code):
+        for command in self.commands:
+            if code == command:
+                return self.execute_command(command) or True
+                
+        return False
 
 
-if __name__ == '__main__':
-    main()
-
+    def run(self):
+        while True:
+            print('>>> ', end = '')
+            code = input()
+            if not self.check_for_command(code): 
+                code = self.clean(code)
+                result = self.executer.interpret(code)
+                print(result)
+                self.executer.cleanup(code)
